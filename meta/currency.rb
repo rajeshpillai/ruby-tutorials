ec202f2ac725ac2e7ce7a638cf3471d8116c33d2
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-17 15:37:46 +0530

    euro/euros both works using gsub

--- meta/currency.rb
+++ meta/currency.rb
@@ -5,19 +5,25 @@ class Numeric
   # capture all args in case we have to call super
   def method_missing(method_id, *args, &block)
     currency = method_id.to_s.gsub(/s$/,'')
+    #puts "method #{method_id}"
+    #puts "currency #{currency}"
+
     if @@currencies.has_key?(currency)
       self / @@currencies[currency]
     else
       super
     end
-
       
   end
+
 end
 
 
 # Test cases
 
 puts "100000 yen is #{100000.yen.round(3)} INR"
+puts "100000 yen is #{100000.yens.round(3)} INR"
 puts "100000 euro is #{100000.euro.round(3)} INR"
 puts "100000 usd is #{100000.usd.round(3)} INR"
+
+
