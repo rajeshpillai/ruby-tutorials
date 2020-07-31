4a34281e3e0ce7b27e08789ba98eb08fc8e5ca87
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-07-31 16:59:15 +0530

    Updated with logs

--- module/module-meta.rb
+++ module/module-meta.rb
@@ -1,5 +1,6 @@
 def custom_extend(module_name)
 	module_name.singleton_methods(false).each do |method|
+		p "Creating method #{method}"
   		define_singleton_method(method) do |*args, &block|
     		module_name.singleton_method(method).call(*args, &block)
   		end
@@ -12,6 +13,11 @@ module Feature1
 	def self.s1(p1)
 		puts "s1 " + p1
 	end
+
+	def self.s2(p1)
+		puts "s2 " + p1
+	end
+
 
 	def f1
 		puts "f1"
@@ -37,3 +43,5 @@ App.f2
 Feature1.s1 ("parameter s1")
 
 App.s1("parameter 1")
+
+p Feature1.singleton_methods(false)
